2dd6bc84be2ce34cb80c560d96200330f6058708
hermann90 <hermannchefouet@gmail.com>
2024-11-14 20:58:33 +0100

    update code

--- main-splunk.tf
+++ main-splunk.tf
@@ -14,7 +14,7 @@ resource "aws_subnet" "web-subnet" {
   vpc_id                  = aws_vpc.my-vpc.id
   cidr_block              = "10.0.1.0/24"
   map_public_ip_on_launch = true
-  availability_zone       = "us-west-2a"
+  availability_zone       = "${var.aws_region}a"
 
   tags = {
     Name = "splunk-subnet"
--- terraform.tfvars
+++ terraform.tfvars
@@ -1,3 +1,3 @@
-aws_region        = "us-west-2"
+aws_region        = "us-east-2"
 aws_instance_type_forwader = "t2.medium"
 aws_instance_type_server = "t2.xlarge"
